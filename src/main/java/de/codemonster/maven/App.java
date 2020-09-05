d629741a4ff287a722d83a80f641a016e9e53770
Jannis Brune <jannis.brune@hs-osnabrueck.de>
2020-09-05 16:41:34 +0200

    Project added

---
+++ src/main/java/de/codemonster/maven/App.java
@@ -0,0 +1,13 @@
+package de.codemonster.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
